42fb34be47276a5b3133293bf1b28f69f7806402
Rafi Long <rafilong42@gmail.com>
2014-08-21 09:58:36 -0700

    Flag: Added JavaDocs to Flag variables and methods

--- flag/src/Flag.java
+++ flag/src/Flag.java
@@ -17,15 +17,23 @@ public class Flag {
      */
     public static Star[] stars = new Star[50];
 
+    /** The width of the smaller stripes in relation to the total height */
     private static double smallStripeWidth = 1.14;
+    /** The width of the larger stripes in relation to the total height */
     private static double largeStripeWidth = 1.9;
+    /** The height in relation to the total height */
     private static double stripeHeight = 1.0 / 13.0;
 
+    /** The width of the union in relation to the total height */
     private static double unionWidth = 0.76;
+    /** The height of the union in relation to the total height */
     private static double unionHeight = 7.0 / 13.0;
 
+    /** The diameter of the star in relation to the total height */
     private static double starDiameter = 0.0616;
+    /** The offset of the star on the x axis in relation to the total height */
     private static double xStarOffset = 0.063;
+    /** The offset of the star on the y axis in relation to the total height */
     private static double yStarOffset = 0.054;
 
 
@@ -35,16 +43,26 @@ public class Flag {
      * @param args unused parameter
      */
     public static void main(String args[]) {
+
     }
 
+    /**
+     * Adds values to all of the stripe objects
+     */
     public static void initStripes() {
 
     }
 
+    /**
+     * Adds values to the union object
+     */
     public static void initUnionBlue() {
 
     }
 
+    /**
+     * Adds values to the star objects
+     */
     public static void initStars() {
 
     }
