cdae2e38e813642be064ffb529bd9baa9dff96a1
Rafi Long <rafilong42@gmail.com>
2014-08-26 09:20:58 -0700

    Flag: Added display constructor

--- flag/src/Display.java
+++ flag/src/Display.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 /**
  * Calls the Rectangle and Star draw functions for all Flag objects
  */
-public class Display {
+public class Display extends JComponent {
+
+    /**
+     * Empty constructor
+     */
+    public Display() {
+
+    }
 }
