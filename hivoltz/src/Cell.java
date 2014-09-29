dfe87c556fed689e688c18a6d5a78cb0123e80ea
Rafi Long <rafilong42@gmail.com>
2014-09-29 09:58:47 -0700

    Hivoltz: Added spawning

--- hivoltz/src/Cell.java
+++ hivoltz/src/Cell.java
@@ -2,14 +2,60 @@
  * @author Rafi Long
  */
 public class Cell {
+    /**
+     * The different types of objects that can fill the cells
+     */
     public enum Type {
         NOTHING, MHO, FENCE, PLAYER;
     }
 
-    public Type type = Type.NOTHING;
+    /** The type of the cell (can change) */
+    private Type type = Type.NOTHING;
+    /** The x coordinate of the cell (doesn't change) */
+    private int x = 0;
+    /** The y coordinate of the clel (doesn't change) */
+    private int y = 0;
 
+    /**
+     * Constructor for cell
+     * @param type the type of the cell
+     * @param x the x coordinate of the cell
+     * @param y the y coordinate of the cell
+     */
     public Cell(Type type, int x, int y) {
         this.type = type;
 
+    }
+
+    /**
+     * Returns the type
+     * @return the type
+     */
+    public Type getType() {
+        return type;
+    }
+
+    /**
+     * Sets the type
+     * @param type the type of the cell
+     */
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    /**
+     * Returns the x
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Returns the y
+     * @return the y
+     */
+    public int getY() {
+        return y;
     }
 }
--- hivoltz/src/Grid.java
+++ hivoltz/src/Grid.java
@@ -13,28 +13,95 @@ public class Grid {
      * A constructor that calls all of the initiative methods
      */
     public Grid() {
+        initCells();
+        initFences();
+        initPlayer();
+        initMhos();
+    }
 
+    /**
+     * Initializes the cell objects
+     */
+    public void initCells() {
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                grid[i][j] = new Cell(Cell.Type.NOTHING, i, j);
+            }
+        }
     }
 
     /**
      * Creates fences around the edge
-     * Randomly spawns the other 12 fences
+     * Randomly spawns the other 20 fences
      */
     public void initFences() {
+        // initializes fences on top and bottom
+        for (int i = 0; i < 12; i++) {
+            grid[i][0].setType(Cell.Type.FENCE);
+            grid[i][11].setType(Cell.Type.FENCE);
+        }
 
+        // initializes fences on left and right
+        for (int i = 1; i < 11; i++) {
+            grid[0][i].setType(Cell.Type.FENCE);
+            grid[11][i].setType(Cell.Type.FENCE);
+        }
+
+        // initializes random fences
+        for (int i = 0; i < 20; i++) {
+            // finds random x and y coordinates between 0 and 12
+            int x = (int)(Math.random() * 12.0);
+            int y = (int)(Math.random() * 12.0);
+
+            // checks if there is something in the cell
+            if (grid[x][y].getType().equals(Cell.Type.NOTHING)) {
+                grid[x][y].setType(Cell.Type.FENCE);
+            // if there is, it doesn't set and adds the number of trials
+            } else {
+                i--;
+            }
+        }
     }
 
     /**
      * Randomly spawns the player in a space with no fence
      */
     public void initPlayer() {
+        // initializes random player
+        for (int i = 0; i < 1; i++) {
+            // finds random x and y coordinates between 0 and 12
+            int x = (int)(Math.random() * 12.0);
+            int y = (int)(Math.random() * 12.0);
 
+            // checks if there is something in the cell
+            if (grid[x][y].getType().equals(Cell.Type.NOTHING)) {
+                grid[x][y].setType(Cell.Type.PLAYER);
+                player = grid[x][y];
+                // if there is, it doesn't set and adds the number of trials
+            } else {
+                i--;
+            }
+        }
     }
 
     /**
-     * Randomly spawns the mhos in spaces that don't have a player or a fence
+     * Randomly spawns the 12 mhos in spaces that don't have a player or a fence
      */
     public void initMhos() {
+        // initializes random player
+        for (int i = 0; i < 12; i++) {
+            // finds random x and y coordinates between 0 and 12
+            int x = (int)(Math.random() * 12.0);
+            int y = (int)(Math.random() * 12.0);
 
+            // checks if there is something in the cell
+            if (grid[x][y].getType().equals(Cell.Type.NOTHING)) {
+                grid[x][y].setType(Cell.Type.MHO);
+                mhos[i] = grid[x][y];
+                // if there is, it doesn't set and adds the number of trials
+            } else {
+                i--;
+            }
+        }
     }
 }
