1ce38d61822001164be7185dc2e1ef7ebc8f815d
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:50:17 -0700

    Hivoltz: Added frame stuff

--- hivoltz/src/Display.java
+++ hivoltz/src/Display.java
@@ -4,9 +4,53 @@
  * See code for documentation
  */
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Displays the board
  * @author Rafi Long
  */
-public class Display {
+public class Display extends JComponent {
+
+    /**
+     * Empty constructor
+     */
+    public Display() {
+
+    }
+
+    /**
+     * Calls the other paint methods
+     * @param g
+     */
+    public void paint(Graphics g) {
+        drawTiles(g);
+        drawPlayer(g);
+        drawMhos(g);
+    }
+
+    /**
+     * Draws the empty tiles and fences
+     * @param g
+     */
+    private void drawTiles(Graphics g) {
+
+    }
+
+    /**
+     * Draws the player
+     * @param g
+     */
+    private void drawPlayer(Graphics g) {
+
+    }
+
+    /**
+     * Draws the mhos
+     * @param g
+     */
+    private void drawMhos(Graphics g) {
+
+    }
 }
--- hivoltz/src/Main.java
+++ hivoltz/src/Main.java
@@ -4,9 +4,39 @@
  * See code for documentation
  */
 
+import javax.swing.*;
+
 /**
- * Calls all methods and controls turn cycle
+ * Creates a new game
+ * Creates frame
  * @author Rafi Long
  */
 public class Main {
+    /** The frame */
+    public static JFrame frame = new JFrame();
+
+    /**
+     * Initializes all methods and begins call cycle
+     * @param args
+     */
+    public static void main(String args[]) {
+        initFrame();
+    }
+
+    /**
+     * Initializes the frame
+     */
+    private static void initFrame() {
+        // creates a new display object
+        Display display = new Display();
+
+        // sets program to terminate on window close
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // sets the size of the frame to be 800 by 800
+        frame.setSize(800, 800);
+        // sets the frame to be visible
+        frame.setVisible(true);
+        // adds the previously created display object to the frame
+        frame.add(display);
+    }
 }
