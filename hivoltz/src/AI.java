198ac3beb039a538396f72c823e7d9eafe86cf1e
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:24:25 -0700

    Hivoltz: Created AI class

---
+++ hivoltz/src/AI.java
@@ -0,0 +1,12 @@
+/*
+ * AP Computer Science Hivoltz Project
+ * Code written by Rafi Long
+ * See code for documentation
+ */
+
+/**
+ * Controls when the mhos move
+ * @author Rafi Long
+ */
+public class AI {
+}
