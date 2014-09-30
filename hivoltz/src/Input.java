9f47a536403c8d2a33a6ae1d1bf4086869b4ba27
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:24:43 -0700

    Hivoltz: Created Input class

---
+++ hivoltz/src/Input.java
@@ -0,0 +1,12 @@
+/*
+ * AP Computer Science Hivoltz Project
+ * Code written by Rafi Long
+ * See code for documentation
+ */
+
+/**
+ * Gets player input and moves the player
+ * @author Rafi Long
+ */
+public class Input {
+}
