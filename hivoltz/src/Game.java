4f4cfeef4613088c84e0960d4c1243b17571642b
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:50:25 -0700

    Hivoltz: Createed Game class

---
+++ hivoltz/src/Game.java
@@ -0,0 +1,14 @@
+/*
+ * AP Computer Science Hivoltz Project
+ * Code written by Rafi Long
+ * See code for documentation
+ */
+
+/**
+ * Creates the new call cycle and grid
+ * @author Rafi Long
+ */
+public class Game {
+    /** The grid */
+    public static Grid grid = new Grid();
+}
