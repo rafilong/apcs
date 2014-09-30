0f07bfaffd90ed7028055b2bdb5e413941a6d47f
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:24:32 -0700

    Hivoltz: Created Display class

---
+++ hivoltz/src/Display.java
@@ -0,0 +1,12 @@
+/*
+ * AP Computer Science Hivoltz Project
+ * Code written by Rafi Long
+ * See code for documentation
+ */
+
+/**
+ * Displays the board
+ * @author Rafi Long
+ */
+public class Display {
+}
