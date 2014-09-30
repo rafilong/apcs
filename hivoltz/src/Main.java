aab197564249f760c1fe08cad17082d7423b5429
Rafi Long <rafilong42@gmail.com>
2014-09-30 09:24:50 -0700

    Hivoltz: Created Main class

---
+++ hivoltz/src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * AP Computer Science Hivoltz Project
+ * Code written by Rafi Long
+ * See code for documentation
+ */
+
+/**
+ * Calls all methods and controls turn cycle
+ * @author Rafi Long
+ */
+public class Main {
+}
