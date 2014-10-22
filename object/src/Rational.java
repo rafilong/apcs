df809790f803617b664dba71dcee5aa63269fbf2
Rafi Long <rafilong42@gmail.com>
2014-10-21 20:34:52 -0700

    Object: Finished question 4

--- object/src/Rational.java
+++ object/src/Rational.java
@@ -30,7 +30,128 @@
  * @author Rafi Long
  *
  * Edits by Rafi:
- * - created all functions and class!
+ * - got functions from https://github.com/42mileslong/intro-java/blob/master/Chapter%2011%20Exercises/src/Rational.java
+ * - made everything object-oriented
  */
 public class Rational {
+    int numerator, denominator;
+
+    /**
+     * A constructor that takes no arguments
+     */
+    public Rational() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    /**
+     * Constructor with arguments
+     * @param d the denominator value
+     * @param n the numerator value
+     */
+    public Rational(int n, int d) {
+        this.numerator = n;
+        this.denominator = d;
+    }
+
+    /**
+     * prints a Rational object
+     */
+    public void printRational() {
+        System.out.println(this.numerator +  "/"  + this.denominator);
+    }
+
+    /**
+     * Reverses the sign of a rational number
+     * Edits the values, so there is no return value
+     */
+    public void negate() {
+        this.numerator = this.numerator * -1;
+        if(this.denominator <= 0) {
+            this.numerator = this.numerator * -1;
+            this.denominator = this.denominator * -1;
+        }
+    }
+
+    /**
+     * Reverses the numerator and the denominator
+     * Edits the values, so there is no return value
+     */
+    public void invert() {
+        int tempN = this.numerator;
+        int tempD = this.denominator;
+
+        this.numerator = tempD;
+        this.denominator = tempN;
+    }
+
+    /**
+     * Converts the rational number to a double
+     * @return the double
+     */
+    public double toDouble() {
+        return ((double) this.numerator) / ((double) this.denominator);
+    }
+
+    /**
+     * Reduces a rational number to its lowest terms
+     * Prints the number
+     *
+     * Note: this function does not change the values or return a function. It only prints the solution.
+     */
+    public void reduce() {
+        int remain, num1, num2;
+        num1 = this.numerator;
+        num2 = this.denominator;
+        remain = num1 % num2;
+        while(remain != 0) {
+            remain = num1 % num2 ;
+            num1 = num2;
+            num2 = remain;
+        }
+        int GCD = num1;
+
+        Rational ret = new Rational(this.numerator/GCD, this.denominator/GCD);
+
+        // invokes printRational (the scope is the object that the function is called on)
+        ret.printRational();
+    }
+
+    /**
+     * Takes 2 rational numbers as arguments and returns a new rational number that is the added values
+     * @param that the number `this` is being added to
+     * @return the combined values of `this` and `that`
+     */
+    public Rational add(Rational that) {
+        Rational retAdd = new Rational(0, 0);
+        if(this.denominator == that.denominator) {
+            retAdd.numerator = this.numerator + that.numerator;
+            retAdd.denominator = this.denominator;
+            return retAdd;
+        } else {
+            retAdd.numerator = this.numerator * that.denominator;
+            retAdd.denominator = this.denominator * that.denominator;
+
+            that.numerator = that.numerator * this.denominator;
+            that.denominator = that.denominator * this.denominator;
+
+            retAdd.numerator += that.numerator;
+
+            int remain, num1, num2;
+            num1 = retAdd.numerator;
+            num2 = retAdd.denominator;
+            remain = num1 % num2;
+            while(remain != 0) {
+                remain = num1 % num2 ;
+                num1 = num2;
+                num2 = remain;
+            }
+            int GCD = num1;
+
+            retAdd.numerator = retAdd.numerator/GCD;
+            retAdd.denominator = retAdd.denominator/GCD;
+
+            return retAdd;
+        }
+    }
 }
