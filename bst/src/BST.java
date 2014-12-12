6f20cb729fd505645d833b34fa1305f4c4072426
Rafi Long <rafilong42@gmail.com>
2014-12-12 10:14:41 -0800

    BST: Implemented various methods

--- bst/src/BST.java
+++ bst/src/BST.java
@@ -2,5 +2,26 @@
  *
  */
 public class BST<T extends Comparable<T>> {
-    
+
+    private BSTnode<T> root;
+
+    public BST() {
+
+    }
+
+    public BSTnode<T> getTree() {
+        return root;
+    }
+
+    public boolean isLeaf() {
+        return root.isLeaf();
+    }
+
+    public void printTree() {
+
+    }
+
+    public String toString() {
+        return root.toString() + " value of " + root.getDatum();
+    }
 }
--- bst/src/BSTnode.java
+++ bst/src/BSTnode.java
@@ -2,5 +2,61 @@
  *
  */
 public class BSTnode<T extends Comparable> {
-    
+
+    private T datum;
+
+    private BSTnode<T> left;
+
+    private BSTnode<T> right;
+
+    public BSTnode(T datum) {
+        this.datum = datum;
+        this.left = null;
+        this.right = null;
+    }
+
+    public T getDatum() {
+        return datum;
+    }
+
+    public BSTnode getLeft() {
+        return left;
+    }
+
+    public BSTnode<T> getRight() {
+        return right;
+    }
+
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+
+    public void insert(T datum) {
+
+    }
+
+    public void setLeft(T datum) {
+        left = new BSTnode<T>(datum);
+    }
+
+    public void setRight(T datum) {
+        right = new BSTnode<T>(datum);
+    }
+
+    public void delete(T datum) {
+
+    }
+
+    public void printTree() {
+
+    }
+
+    public int depth() {
+        if (this.isLeaf()) return 1;
+        return Math.max(left.depth(), right.depth()) + 1;
+    }
+
+    public String toString() {
+        return datum.toString();
+    }
 }
